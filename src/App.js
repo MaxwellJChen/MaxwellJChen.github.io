3521c266062c2c13b6f810bb4e94f3890bae247d
Maxwell Jack Chen <maxwelljchen1@gmail.com>
2024-03-14 23:37:54 -0500

    Changed to using HashRouter instead of BrowserRouter to fix GitHub Pages refresh problem.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar.jsx';
 import Home from './pages/home.jsx';
 import Projects from './pages/projects.jsx';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter basename='/'>
       <Routes>
-        <Route path="/" element={<Navbar />}>
+        <Route path='/' element={<Navbar />}>
           <Route index element={<Home />} />
           <Route path="projects" element={<Projects />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
