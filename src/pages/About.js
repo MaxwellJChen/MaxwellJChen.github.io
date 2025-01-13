addb0bf8ecdb20599394d948ef4d4edf4501f9c4
MaxwellJChen <maxwelljchen1@gmail.com>
2025-01-12 19:25:29 -0600

    preliminary reordering of projects section, need to reorganize better and add animation

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,20 +4,13 @@ import Gradient from './../components/Gradient';
 import Slider from './../components/Slider';
 import target from './../assets/icons/target.svg';
 import medal from './../assets/icons/medal.svg';
-import northwestern from './../assets/logos/northwestern.svg';
-import column from './../assets/icons/column.svg';
-import diploma from './../assets/icons/diploma.svg';
 import mortar from './../assets/icons/mortar-hat.svg';
-import pencil from './../assets/icons/pencil.svg';
 
 
 function About() {
     return (
         <div className='glass section' id='about'>
-            <Gradient gradient={
-                'linear-gradient(to right top, #d93c3c, #e22660, #df1f87, #cd34b0, #a74fd7, #8071ef, #508afc, #009fff, #00baff, #00cde4, #00d995, #23dd1e)'
-            } rotate={false} />
-
+            {/* <Gradient /> */}
             <div className='container'>
                 <h2>About</h2>
                 <div className='vert'>
@@ -51,7 +44,7 @@ function About() {
                             </h4>
                         </div>
 
-                        <Slider text={'pursuing combined Computer Science Master\'s and Bachelor\'s and Math Bachelor\'s.'}/>
+                        <Slider text={'pursuing combined Computer Science Master\'s and Bachelor\'s and Mathematics Bachelor\'s at Northwestern.'}/>
                     </div>
                 </div>
             </div>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,109 +4,156 @@ import link from './../assets/icons/link.svg';
 import pill from './../assets/icons/pill.svg';
 import brain from './../assets/icons/brain.svg';
 import sound from './../assets/icons/sound.svg';
-import stock from './../assets/icons/stock.svg';
+import stocks from './../assets/icons/stocks.svg';
+import snp from './../assets/images/snp.png';
+import github from './../assets/logos/github.svg';
+import poster from './../assets/icons/poster.svg';
+import slides from './../assets/icons/slides.svg';
+import document from './../assets/icons/document.svg';
 
 function Projects() {
     return (
         <div className='glass section' id='projects'>
-            <Gradient gradient={
-                `radial-gradient(at 40% 20%, hsla(28,100%,74%,1) 0px, hsla(189,100%,56%,1) 50%),
-                radial-gradient(at 80% 0%, hsla(189,100%,56%,1) 0px, hsla(189,100%,56%,1) 50%),
-                radial-gradient(at 0% 50%, hsla(355,100%,93%,1) 0px, hsla(189,100%,56%,1) 50%),
-                radial-gradient(at 80% 50%, hsla(340,100%,76%,1) 0px, hsla(189,100%,56%,1) 50%),
-                radial-gradient(at 0% 100%, hsla(22,100%,77%,1) 0px, hsla(189,100%,56%,1) 50%),
-                radial-gradient(at 80% 100%, hsla(242,100%,70%,1) 0px, hsla(189,100%,56%,1) 50%),
-                radial-gradient(at 0% 0%, hsla(343,100%,76%,1) 0px, hsla(189,100%,56%,1) 50%)`
-            } />
-
-            <div className='container'>
             <h2>Projects</h2>
 
             <div className='vert'>
-                <div className='glass panel'>
-                    <div className='hor'>
-                        <div>
-                            <img src={stock} style={{'width': '30px', 'marginRight': '0.4vw'}} alt='Stocks Symbol' />
-                            Correlation-Driven Feature Extraction for S&P 500 Prediction with Machine Learning
-                        </div>
-                        Nov 2024 — Dec 2024
-                    </div>
 
-                    <ul>
-                        <li>
-                            <img src={link} style={{'width': '30px'}} alt='Link Symbol' />
-                            <a href='https://docs.google.com/presentation/d/19oBmw7dU6k7OxufPLGutSmjoAS_NSEhYLOFknppCA-U/edit?usp=sharing' target='blank'>Slides</a>
-                        </li>
-                        <li>
-                            Predicted whether S&P 500 closing prices would increase or decrease based on prior S&P 500 data in team of 5 and 
-                            single-handedly extracted features for training resulting in 17% neural network accuracy improvement from 56% to 73%
-                        </li>
-                    </ul>
+                <div id='stocks'>
+                    <Gradient />
+                    <div className='vert project'>
+                        <div className='hor'>
+                            <img src={stocks} alt='Stocks Symbol' />
+                            Nov 2024 — Dec 2024
+                        </div>
+
+                        <div className='glass panel'>
+                            <h4>Correlation-Driven Feature Extraction for S&P 500 Prediction with Machine Learning</h4>
+                        </div>
+
+                        <div className='links'>
+                            <div className='hor'>
+                                <img id='link' src={link} alt='Link Symbol' />
+                                <a href='https://docs.google.com/presentation/d/19oBmw7dU6k7OxufPLGutSmjoAS_NSEhYLOFknppCA-U/edit?usp=sharing' target='blank' title='Slides'>
+                                    <img src={slides} alt='Slides Symbol' />
+                                </a>
+                            </div>
+                        </div>
+
+                        <span>
+                            Extracted a suite of features for S&P 500 stock prediction, single-handedly increasing neural network performance by 17%
+                        </span>
+
+                        {/* <div className='images'>
+                            <img src={snp} alt='S&P Chart' />
+                        </div> */}
+                    </div>
                 </div>
 
-                <div className='glass panel'>
-                    <div className='hor'>
-                        <div>
-                            <img src={sound} style={{'width': '30px', 'marginRight': '0.5vw'}} alt='Sound Symbol' />
-                            Identifying Speech Disfluencies with Weakly Supervised Audio Transformers
-                        </div>
-                        Jan 2024 — Apr 2024
-                    </div>
 
-                    <ul>
-                        <li>
-                            <img src={link} style={{'width': '30px'}} alt='Link Symbol' />
-                            <a href='https://docs.google.com/document/d/1AM16Qcufp2CDrQ7Hu9Fd0Sxu-RxArqRE/edit?usp=sharing&ouid=109208091310666571454&rtpof=true&sd=true' target='blank'>Proposal</a>, <a href='https://docs.google.com/presentation/d/1ym_tdhuEXcM3D7PPiYlFOMQvvP1Bjhp1bxjz7sdi4v0/edit?usp=sharing' target='blank'>Presentation</a>
-                        </li>
-                        <li>
-                            ASHA (premier speech-language conference) proposal accepted for training audio transformers to 
-                            identify children who stutter and label speech disfluencies with custom dataset from Developmental 
-                            Stuttering Lab
-                        </li>
-                    </ul>
+                <div id='speech-disfluencies'>
+                    <Gradient />
+                    <div className='vert project'>
+                        <div className='hor'>
+                            <img src={sound} alt='Speech Symbol' />
+                            Jan 2024 — Apr 2024
+                        </div>
+
+                        <div className='glass panel'>
+                            <h4>Identifying Speech Disfluencies with Weakly Supervised Audio Transformers</h4>
+                        </div>
+
+                        <div className='links'>
+                            <div className='hor'>
+                                <img id='link' src={link} alt='Link Symbol' />
+                                <a href='https://docs.google.com/document/d/1AM16Qcufp2CDrQ7Hu9Fd0Sxu-RxArqRE/edit?usp=sharing&ouid=109208091310666571454&rtpof=true&sd=true' target='blank' title='Proposal'>
+                                    <img src={document} alt='Document Symbol' />
+                                </a>
+
+                                <a href='https://docs.google.com/presentation/d/1ym_tdhuEXcM3D7PPiYlFOMQvvP1Bjhp1bxjz7sdi4v0/edit?usp=sharing' target='blank' title='Slides'>
+                                    <img src={slides} alt='Slides Symbol' />
+                                </a>
+                            </div>
+                        </div>
+                        
+                        <span>
+                            Trained deep audio transformers on Vanderbilt Developmental Stuttering Lab dataset to identify instances of stuttering in 
+                            child speech
+                        </span>
+                    </div>
                 </div>
 
-                <div className='glass panel'>
-                    <div className='hor'>
-                        <div>
-                            <img src={pill} style={{'width': '30px', 'marginRight': '0.5vw'}} alt='Pill Symbol' />
-                            SURGE: Sequential Reinforcement Graph Agents for Accelerated Therapeutic Design
+                <div id='surge'>
+                    <Gradient />
+                    <div className='vert project'>
+                        <div className='hor'>
+                            <img src={pill} alt='Pill Symbol' />
+                            Jun 2022 - Nov 2023
                         </div>
-                        Jun 2022 - Apr 2024
-                    </div>
 
-                    <ul>
-                        <li>
-                            <img src={link} style={{'width': '30px'}} alt='Link Symbol' />
-                            <a href='https://github.com/MaxwellJChen/RL_Drug_Generation' target='blank'>GitHub</a>, <a href='https://docs.google.com/presentation/d/1t4zp9FhjKXMU1bKOA7R_HRIAx7G7S4ZU2srLXNw2FrA/edit?usp=sharing' target='blank'>Poster</a>, <a href='https://docs.google.com/presentation/d/1olUNJCFUpsFnzMUJdOc2ipAIsVjbMunaQKxM8u9Sz0E/edit?usp=sharing' target='blank'>Presentation</a>
-                        </li>
-                        <li>
-                            Designed carbon chains and drug-like, synthetically accessible molecules with reinforcement learning 
-                            (proximal policy optimization, imitation learning, custom molecular environment) and graph neural networks
-                        </li>
-                    </ul>
+                        <div className='glass panel'>
+                            <h4>SURGE: Sequential Reinforcement Graph Agents for Accelerated Therapeutic Design</h4>
+                        </div>
+
+                        <div className='links'>
+                            <div className='hor'>
+                                <img id='link' src={link} alt='Link Symbol' />
+
+                                <a href='https://github.com/MaxwellJChen/RL_Drug_Generation' target='blank' title='GitHub Repository'>
+                                    <img src={github} alt='GitHub' />
+                                </a>
+                                
+                                <a href='https://docs.google.com/presentation/d/1t4zp9FhjKXMU1bKOA7R_HRIAx7G7S4ZU2srLXNw2FrA/edit?usp=sharing' target='blank' title='Poster'>
+                                    <img src={poster} alt='Poster Symbol' />
+                                </a>
+                                
+                                <a href='https://docs.google.com/presentation/d/1olUNJCFUpsFnzMUJdOc2ipAIsVjbMunaQKxM8u9Sz0E/edit?usp=sharing' target='blank' title='Slides'>
+                                    <img src={slides} alt='Slides Symbol' />
+                                </a>
+                            </div>
+                        </div>
+
+                        <span>
+                            Designed carbon chains and drug-like, synthetically accessible molecules with reinforcement learning (proximal policy optimization, 
+                            imitation learning, custom molecular environment) and graph neural networks
+                        </span>
+                    </div>
                 </div>
 
-                <div className='glass panel'>
-                    <div className='hor'>
-                        <div>
-                            <img src={brain} style={{'width': '30px', 'margin': '0 0.6vw 0 0'}} alt='Brain Symbol' />
-                            Novel Feature Extraction and Noise Removal Techniques for Imagined Speech Decoding
+                <div id='imagined-speech-decoding'>
+                    <Gradient />
+                    <div className='vert project'>
+                        <div className='hor'>
+                            <img src={brain} alt='Brain Symbol' />
+                            Sep 2021 - Mar 2022
                         </div>
-                        Sep 2021 - Mar 2022
-                    </div>
 
-                    <ul>
-                        <li>
-                            <img src={link} style={{'width': '30px'}} alt='Link Symbol' />
-                            <a href='https://docs.google.com/document/d/1akev5uc0mToU7PbgaWh9qRYgY3eeEqFochB3pevUsJQ/edit?usp=sharing' target='blank'>Abstract</a>, <a href='https://drive.google.com/file/d/1wEBQFb1DBjf_zdExnNkXNtYeDLloo2om/view?usp=sharing' target='blank'>Slides</a>, <a href='https://drive.google.com/file/d/1glPcHmMthblDJouHRGcb1hTq7Y2I9Rt-/view?usp=sharing' target='blank'>Poster</a>
-                        </li>
-                        <li>
-                            Classified which of 3 words subjects were thinking of based on EEG brainwaves with deep ANNs, achieving SOTA 53.1% accuracy with novel feature extraction and noise reduction combination
-                        </li>
-                    </ul>
+                        <div className='glass panel'>
+                            <h4>Novel Feature Extraction and Noise Removal Techniques for Imagined Speech Decoding</h4>
+                        </div>
+
+                        <div className='links'>
+                            <div className='hor'>
+                                <img id='link' src={link} alt='Link Symbol' />
+                                <a href='https://docs.google.com/document/d/1akev5uc0mToU7PbgaWh9qRYgY3eeEqFochB3pevUsJQ/edit?usp=sharing' target='blank' title='Abstract'>
+                                    <img src={document} alt='Document Symbol' />
+                                </a>
+
+                                <a href='https://drive.google.com/file/d/1wEBQFb1DBjf_zdExnNkXNtYeDLloo2om/view?usp=sharing' target='blank' title='Slides'>
+                                    <img src={slides} alt='Slides Symbol' />
+                                </a>
+
+                                <a href='https://drive.google.com/file/d/1glPcHmMthblDJouHRGcb1hTq7Y2I9Rt-/view?usp=sharing' target='blank' title='Poster'>
+                                    <img src={poster} alt='Poster Symbol' />
+                                </a>
+                            </div>
+                        </div>
+
+                        <span>
+                            Classified which of 3 words subjects were thinking about based on their brainwaves, achieving state-of-the-art 
+                            53.1% accuracy with novel feature extraction and noise reduction combination
+                        </span>
+                    </div>
                 </div>
-            </div>
             </div>
         </div>
     )
